5262e0446c8bcd70315b3e60c7dac0d611e7cee4
honyi <2394486969@qq.com>
2023-09-17 16:14:54 +0800

    罗马数转整数

---
+++ learnOfalgorithm/RomaToInt/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace RomaToInt
+{
+    //罗马数字转整数
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+public class Solution
+    {
+    int romanToInt(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>()
+            {
+                {'I',1 },
+                {'V',5 },
+                {'X',10 },
+                {'L',50 },
+                {'C',100 },
+                {'D',500 },
+                {'M',1000 }
+            };
+            int res = 0;
+            for(int i=0;i<s.Length;i++)
+            {  
+                if(i<s.Length-1 && dic[s[i]] < dic[s[i + 1]])
+                {
+                    res -= dic[s[i]];
+                }
+                else
+                {
+                    res += dic[s[i]];
+                }
+            }
+            return res;
+        }
+    };
+}
