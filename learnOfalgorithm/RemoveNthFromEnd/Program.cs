c0ce5f1a87e1c7c9d00de7b11fcbf1d751a78b1e
honyi <2394486969@qq.com>
2023-09-19 21:15:06 +0800

    删除链表的倒数第n个节点

---
+++ learnOfalgorithm/RemoveNthFromEnd/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace RemoveNthFromEnd
+{
+
+    //给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode result = new Solution().RemoveNthFromEnd(new ListNode(1, null), 1);
+            while (result != null)
+            {
+                Console.WriteLine(result.val);
+                result = result.next;
+            } 
+        }
+    }
+
+
+
+public class ListNode
+ {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+     {
+         this.val = val;
+         this.next = next;
+              }
+}
+
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            int count = 0;
+            ListNode FirstNode = head;
+            while(head != null)
+            {
+                head = head.next;
+                count++;
+            }
+            if(count==n)
+            {
+                return FirstNode.next;
+            }
+            head=FirstNode;
+            for(int i = 0;i<count;i++)
+            {
+                if(i==count-n-1) 
+                { 
+
+                head.next=head.next.next;
+                    break;
+                }
+                head = head.next;
+            }
+            return FirstNode;
+        }
+    }
+
+}
