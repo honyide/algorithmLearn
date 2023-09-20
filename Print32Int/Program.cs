7e7f240b80134335e798e0a380ecd8c8063b68bc
honyi <2394486969@qq.com>
2023-09-20 20:48:05 +0800

    把整数输出为32为二进制

---
+++ Print32Int/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Print32Int
+{
+    //把一个整数输出为32位二进制
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             new Solution().Print32Int(1);
+           Console.ReadKey();
+        }
+    }
+
+    public class Solution
+    {
+        public void Print32Int(int num)
+        {
+            for(int i=31; i>=0; i--)
+            {
+                Console.Write((num&(1<<i)) ==0 ? '0' : '1');
+            }
+            Console.WriteLine();
+        }
+    }
+}
